5e8e88ab8467ce813d03050bb48f23e3f9e1f367
William Bailey <wbaileyfletcher@gmail.com>
2025-09-02 10:55:09 -0400

    Initial Xcode project setup

---
+++ foodTracker/foodTracker/foodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodTrackerApp.swift
+//  foodTracker
+//
+//  Created by William Bailey on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct foodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ foodTracker/foodTrackerTests/foodTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  foodTrackerTests.swift
+//  foodTrackerTests
+//
+//  Created by William Bailey on 9/2/25.
+//
+
+import Testing
+@testable import foodTracker
+
+struct foodTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
